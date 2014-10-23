c43e36cf7fb1b4e829f7795f0b6c78deb281c545
alixander <alexanderw@berkeley.edu>
2014-10-22 17:47:22 -0700

    Initial creation of Card app models and views
    - Added models of Tag, Card, and Contact
    - Added view method for home and get contacts based on card id

--- src/card/models.py
+++ src/card/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Card(models.Model):
+    unique_id = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
+# Could be a different model if we want to later store additional info per company
+    associated_company = models.CharField()
+
+    def __str__(self):
+        return "A card for " + associated_company
+
+class Tag(models.Model):
+    tag = models.CharField(unique=True)
+    tagged_card = models.ForeignKey(Card)
+
+    def __str__(self):
+        return tag
+
+class Contact(models.Model):
+    name = models.CharField(blank=False)
+    phone = models.CharField(blank=False)
+    date = models.DateField(blank=False)
+    associated_card = models.ForeignKey(Card)
+
+    def __str__(self):
+        return "Contact with name " + name
--- src/card/views.py
+++ src/card/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from card.models import *
+import json
+from django.core import serializers
 
 # Create your views here.
+def home(request):
+    return render('index.html', {})
+
+# Return the contact information in JSON given a card ID
+def get_contacts(request):
+    card_id = request.GET.get('card_id')
+    card = Card.objects.filter(unique_id=card_id)
+    contacts = Contact.objects.all().filter(associated_card=card)
+    contacts_output = serializers.serialize("json", contacts)
+    context = RequestContext(request, {
+        'contacts': contacts_output,
+    })
+    return render('index.html', context)
