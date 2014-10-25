f377e95cfeb2bfe035aca1ec01434bee1db1a584
alixander <alexanderw@berkeley.edu>
2014-10-24 20:38:10 -0700

    change user model importing

--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-import card.models import *
+from card.models import *
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="profile")
