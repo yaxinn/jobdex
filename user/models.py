2a93f6a7b0107acf58fe508b3358d2aecf690f83
alixander <alexanderw@berkeley.edu>
2014-10-24 20:38:10 -0700

    change user model importing

--- user/models.py
+++ user/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-import card
-
+import card.models import *
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="profile")
-    companies = models.ManyToManyField(card.models.Company)
+    companies = models.ManyToManyField(Company)
     def __str__(self):
         return "%s's profile" % self.user
 
