fc81b327dc3bfe74114edb835faeefcb8cfbd290
Paulina Ramos <m.paulina.ramos@gmail.com>
2014-10-24 21:08:21 -0700

    Modify unit tests and add functional tests

---
+++ functionalTests.py
@@ -0,0 +1,56 @@
+"""Functional tests."""
+
+import unittest
+import os
+import testLib
+
+class TestCard(testLib.RestTestCase):
+    """Tests Card controllers"""
+
+    # def testCardCreate(self):
+    #     jsonObj = {
+    #                 'companyName' : 'Amazon',
+    #                 'status' : 'in-progress',
+    #                 'jobTitle' : 'Software Engineer',
+    #                 'tags' : 'tech',
+    #                 'contactName' : 'Bob',
+    #                 'contactEmail' : 'bobsmith@gmail.com',
+    #                 'contactPhone' : '5101234567'
+    #               }
+    #     respData = self.makeRequest("/user/create-card",
+    #                                 method = "POST",
+    #                                 data = jsonObj)
+    #     expectedResponse = {'card_id': card_id, 'error_code': 1}
+    #     self.assertDictEqual(respData, expectedResponse)
+
+
+    def testModifyCardStatus(self):
+        self.tags.create_card("CompanyJ", "Software Engineer", "accepted")
+        card_id = str(models.Card.objects.get(name='CompanyJ').id)
+        new_status = "rejected"
+        jsonObj = {
+                    'card_id' : card_id,
+                    'status' : new_status
+                  }
+        respData = self.makeRequest("/card/" + card_id + "/change-status",
+                                    method = "POST",
+                                    data = jsonObj)
+        expectedResponse = {'error_message': 1}
+        self.assertDictEqual(respData, expectedResponse)
+
+class TestUser(testLib.RestTestCase):
+    """Tests User controllers"""
+
+    def testModifyCardStatus(self):
+        self.tags.create_card("CompanyJ", "Software Engineer", "accepted")
+        card_id = str(models.Card.objects.get(name='CompanyJ').id)
+        new_status = "rejected"
+        jsonObj = {
+                    'card_id' : card_id,
+                    'status' : new_status
+                  }
+        respData = self.makeRequest("/card/" + card_id + "/change-status",
+                                    method = "POST",
+                                    data = jsonObj)
+        expectedResponse = {'error_message': 1}
+        self.assertDictEqual(respData, expectedResponse)
