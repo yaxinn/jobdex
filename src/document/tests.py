75d3267ccf105933a67f44674cbd6cf15db430a5
alixander <alexanderw@berkeley.edu>
2014-11-20 18:01:23 -0800

    Unit test for document

--- src/document/tests.py
+++ src/document/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from document.models import *
+from document.views import *
+import string
+import random
 
-# Create your tests here.
+ERROR_CODES = {
+        "SUCCESS": 1
+        }
+
+class DocAddTestCase(TestCase):
+    def setUp(self):
+        client = Client()
+        user_info = {
+            "username": "seth",
+            "password": "tawfik",
+            "confirm_password": "tawfik",
+            "email": "paulina@bev.com",
+        }
+        client.post('/signup/', user_info)
+        data = {
+            "name":"test_file",
+            "pdf": open('document/test.pdf')
+        }
+        self.response = client.post('/api/document/upload/', data)
+
+    def test_analyze_response(self):
+        error_code = json.loads(self.response.content)['error_message']
+        self.assertEqual(error_code, ERROR_CODES['SUCCESS'])
+
