7d7253c0a7624dc2aa26994111fa5e8d664d9f49
Paulina Ramos <m.paulina.ramos@gmail.com>
2014-11-21 22:45:07 -0800

    Integrate new Deck and Card logic with Seth and Paul's features and add more unit tests

--- src/card/tests.py
+++ src/card/tests.py
@@ -6,14 +6,15 @@ from django.core import serializers
 
 ERROR_CODES = {
         "SUCCESS": 1,
-        "CARD_DOESNT_EXIST": -8
+        "CARD_DOESNT_EXIST": -8,
+        "DECK_DOESNT_EXIST": -22
         }
 
 ############
 #   DECK   #
 ############
 
-class DeckCreate(TestCase):
+class CreateDeck(TestCase):
     def setUp(self):
         client = Client()
         user_info = {
@@ -33,6 +34,55 @@ class DeckCreate(TestCase):
         error_code = json.loads(self.response.content)['error_message']
         self.assertEqual(error_code, ERROR_CODES['SUCCESS'])
 
+class RemoveExistingDeck(TestCase):
+    def setUp(self):
+        client = Client()
+        user_info = {
+            "username": "userAA",
+            "password": "password",
+            "confirm_password": "password",
+            "email": "paulina@cool.com",
+        }
+        client.post('/signup/', user_info)
+        deck_data = {
+            "companyName": "Google",
+            "companyDescription": "tech"
+        }
+        self.create_deck_response = client.post('/api/user/create-deck/', deck_data)
+        deck_id = json.loads(self.create_deck_response.content)['deck_id']
+
+        data = {
+            "deck_id": deck_id
+        }
+
+        self.response = client.post('/api/user/delete-deck/', data)
+
+    def test_analyze_response(self):
+        error_code = json.loads(self.response.content)['error_message']
+        self.assertEqual(error_code, ERROR_CODES['SUCCESS'])
+
+class RemoveNonexistentDeck(TestCase):
+    def setUp(self):
+        client = Client()
+        user_info = {
+            "username": "userAAA",
+            "password": "password",
+            "confirm_password": "password",
+            "email": "paulina@cool.com",
+        }
+        client.post('/signup/', user_info)
+
+        # fake data_id
+        data = {
+            "deck_id": "123"
+        }
+
+        self.response = client.post('/api/user/delete-deck/', data)
+
+    def test_analyze_response(self):
+        error_code = json.loads(self.response.content)['error_message']
+        self.assertEqual(error_code, ERROR_CODES['DECK_DOESNT_EXIST'])
+
 ############
 #   CARD   #
 ############
@@ -47,7 +97,16 @@ class AddCard(TestCase):
             "email": "paulina@cool.com",
         }
         client.post('/signup/', user_info)
+
+        deck_data = {
+            "companyName": "Google",
+            "companyDescription": "tech"
+        }
+        self.create_deck_response = client.post('/api/user/create-deck/', deck_data)
+        deck_id = json.loads(self.create_deck_response.content)['deck_id']
+
         data = {
+            "deck_id": deck_id,
             "jobTitle": "Software Engineer",
             "status": "interested",
             "notes": "hello",
@@ -72,7 +131,16 @@ class AddCard(TestCase):
 #             "email": "paulina@cool.com",
 #         }
 #         client.post('/signup/', user_info)
+#
+#         deck_data = {
+#             "companyName": "Google",
+#             "companyDescription": "tech"
+#         }
+#         self.create_deck_response = client.post('/api/user/create-deck/', deck_data)
+#         deck_id = json.loads(self.create_deck_response.content)['deck_id']
+#
 #         card_data = {
+#             "deck_id": deck_id,
 #             "jobTitle": "Software Engineer",
 #             "status": "interested",
 #             "notes": "hello",
@@ -81,31 +149,39 @@ class AddCard(TestCase):
 #             "contactEmail": "poop@gmail.com",
 #             "contactPhone": "123456"
 #         }
-#         self.response = client.post('/api/card/add-card/', card_data)
 #
-#         card_id_data = {
-#             "card_id": "123"
+#
+#         self.add_card_response = client.post('/api/card/add-card/', card_data)
+#         card_id = json.loads(self.add_card_response.content)['card_id']
+#         data = {
+#             "card_id": card_id
 #         }
-#         self.response = client.post('/api/card/remove-card/', data)
+#
+#         self.response = client.post('/api/user/remove-card/', data)
+#
 #
 #     def test_analyze_response(self):
 #         error_code = json.loads(self.response.content)['error_message']
-#         self.assertEqual(error_code, ERROR_CODES['CARD_DOESNT_EXIST'])
+#         self.assertEqual(error_code, ERROR_CODES['SUCCESS'])
 #
 # class RemoveNonexistentCard(TestCase):
 #     def setUp(self):
 #         client = Client()
 #         user_info = {
-#             "username": "userD",
+#             "username": "userBBB",
 #             "password": "password",
 #             "confirm_password": "password",
 #             "email": "paulina@cool.com",
 #         }
 #         client.post('/signup/', user_info)
+#
+#         # fake card_id
 #         data = {
 #             "card_id": "123"
 #         }
-#         self.response = client.post('/api/card/remove-card/', data)
+#
+#         self.response = client.post('/api/user/remove-card/', data)
+#
 #
 #     def test_analyze_response(self):
 #         error_code = json.loads(self.response.content)['error_message']
--- src/card/views.py
+++ src/card/views.py
@@ -25,23 +25,96 @@ def about(request):
 def report(request):
     context = {}
     return render(request, 'report.html', context)
- 
-def get_all_cards(request):  #NOT NEEDED
+
+def get_all_cards(request):
     cards = Card.objects.all()
     cards_output = serializers.serialize("json", cards)
     return JsonResponse(cards_output, safe=False)
 
 # Return all cards of a company, given company name
-def get_company_cards(request):  #NOT NEEDED
+def get_company_cards(request):
     company_name = request.GET.get('company_name')
     cards = Card.objects.filter(associated_company=company_name)
     cards_output = serializers.serialize("json", cards)
     return JsonResponse(cards_output, safe=False)
 
+#############################
+# NEW DECK AND CARDS DESIGN #
+#############################
+
+# Add card given company name, status, tags, and contact info
+@csrf_exempt
+def create_deck(request):
+    company_name = request.POST['companyName']
+    company_description = request.POST['companyDescription']
+    user = UserProfile.objects.get(username=request.user)
+
+    try:
+        company = Company.objects.get(name=company_name)
+    except Company.DoesNotExist:
+        company = Company(name=company_name, description=company_description)
+        company.save()
+
+    new_deck = Deck(associated_company=company, owner=user)
+    new_deck.save()
+
+    deck_id = str(new_deck.unique_id)
+    response = {'deck_id': deck_id, 'error_message': 1}
+    return JsonResponse(response, safe=False)
+
+ #Adding a position to the deck
+@csrf_exempt
+def add_card(request):
+    deck_id = request.POST['deck_id']
+    deck = Deck.objects.get(unique_id=deck_id)
+    job_title = request.POST['jobTitle']
+    status = request.POST['status']
+    notes = request.POST['notes']
+    tags = request.POST['tags'].split(',')
+    contact_name = request.POST['contactName']
+    contact_email = request.POST['contactEmail']
+    contact_phone = request.POST['contactPhone']
+    new_card = Card(job_title=job_title, status=status, notes=notes, card_deck=deck)
+    new_card.save()
+
+    new_contact = Contact(name=contact_name, phone=contact_phone, email=contact_email, associated_card=new_card)
+    new_contact.save()
+
+    for tag in tags:
+        new_tag = Tag(tag=tag.strip(), tagged_card=new_card)
+        new_tag.save()
+    card_id = str(new_card.card_id)
+    response = {'card_id': card_id, 'error_message': 1}
+    return JsonResponse(response, safe=False)
+
+# Remove deck, given a deck id
+@csrf_exempt
+def delete_deck(request):
+    try:
+        deck_id = request.POST['deck_id']
+        Deck.objects.get(unique_id=deck_id).delete()
+        return JsonResponse({'error_message': 1}, safe=False)
+    except Deck.DoesNotExist:
+        return JsonResponse({'error_message': -22}, safe=False)
+
+# Remove card, given a card id
+@csrf_exempt
+def remove_card(request):
+    try:
+        card_id = request.POST['card_id']
+        Card.objects.get(card_id=card_id).delete()
+        return JsonResponse({'error_message': 1}, safe=False)
+    except Card.DoesNotExist:
+        return JsonResponse({'error_message': -8}, safe=False)
+
+###################
+# CARD ATTRIBUTES #
+###################
+
 # Return the contact information in JSON given a card ID
 def get_contacts(request):
     card_id = request.GET.get('card_id')
-    card = Card.objects.filter(unique_id=card_id)
+    card = Card.objects.filter(card_id=card_id)
     contacts = Contact.objects.all().filter(associated_card=card)
     contacts_output = serializers.serialize("json", contacts)
     return JsonResponse(contacts_output, safe=False)
@@ -62,17 +135,15 @@ def remove_contact(request):
 @csrf_exempt
 def edit_contact(request):
     try:
-        card_id = request.GET.get('card_id')
-        contact_name = request.POST.get('contactName')
-        contact_email = request.POST.get('contactEmail')
-        contact_phone = request.POST.get('contactPhone')
-        contact_title = request.POST.get('contactTitle')
-        contact = Contact.objects.filter(name=contact_name, tagged_card=card_id)
-        contact.name = contact_name
-        contact.email = contact_email
-        contact.phone = contact_phone
-        contact.title = contact_title
-        contact.save()
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        new_name = info['new_name']
+        new_email = info['new_email']
+        new_phone = info['new_phone']
+        current_name = info['current_name']
+        card = Card.objects.get(card_id=card_id)
+        contact = Contact.objects.filter(name=current_name, associated_card=card)
+        contact.update(name=new_name, email=new_email, phone=new_phone)
         return JsonResponse({'error_message': 1}, safe=False)
     except Card.DoesNotExist:
         return JsonResponse({'error_message': -8}, safe=False)
@@ -81,61 +152,36 @@ def edit_contact(request):
 
 # Add card given company name, status, tags, and contact info
 @csrf_exempt
-def create_deck(request):
+def create_card(request):
     info = json.loads(request.POST.keys()[0])
     company_name = info['companyName']
-    company_description = info['companyDescription']
-    user = UserProfile.objects.get(username=request.user)
-
-    try:
-        company = Company.objects.get(name=company_name)
-    except Company.DoesNotExist:
-        company = Company(name=company_name, description=company_description)
-        company.save()
-
-    new_deck = Deck(associated_company=company, owner=user)
-    new_deck.save()
-
-    deck_id = str(new_deck.unique_id)
-    response = {'deck_id': deck_id, 'error_message': 1}
-    return JsonResponse(response, safe=False)
-
- #Adding a position to the deck
-@csrf_exempt
-def add_card(request):
-    info = json.loads(request.POST.keys()[0])
-    deck_id = info['deck_id']
-    deck = Deck.objects.get(unique_id=deck_id) 
-    job_title = info['jobTitle']
     status = str(info['status'])
+    job_title = info['jobTitle']
     notes = str(info['notes'])
     tags = str(info['tags']).split(',')
     contact_name = str(info['contactName'])
     contact_email = str(info['contactEmail'])
     contact_phone = str(info['contactPhone'])
-    new_card = Card(job_title=job_title, status=status, notes=notes, card_deck=deck)
+    user = UserProfile.objects.get(username=request.user)
+
+    try:
+        company = Company.objects.get(name=company_name)
+    except Company.DoesNotExist:
+        company = Company(name=company_name)
+        company.save()
+
+    new_card = Card(associated_company=company, status=status, job_title=job_title, notes=notes, owner=user)
     new_card.save()
+
     new_contact = Contact(name=contact_name, phone=contact_phone, email=contact_email, associated_card=new_card)
     new_contact.save()
+
     for tag in tags:
         new_tag = Tag(tag=tag.strip(), tagged_card=new_card)
         new_tag.save()
     card_id = str(new_card.card_id)
     response = {'card_id': card_id, 'error_message': 1}
     return JsonResponse(response, safe=False)
-
-# Remove deck, given a deck id
-@csrf_exempt
-def delete_deck(request):
-    try:
-        info = json.loads(request.POST.keys()[0])
-        deck_id = info['deck_id']
-        Deck.objects.filter(unique_id=deck_id).delete()
-        return JsonResponse({'error_message': 1}, safe=False)
-    except Deck.DoesNotExist:
-        return JsonResponse({'error_message': -22}, safe=False)
-
-   
 
 # Remove card, given a card id
 @csrf_exempt
@@ -151,34 +197,41 @@ def remove_card(request):
 # Add contact given card id and contact info
 @csrf_exempt
 def add_contact(request):
-    card_id = request.GET.get('card_id')
-    card = Card.objects.filter(unique_id=card_id)
-    contact_name = request.POST.get('contact_name')
-    contact_email = request.POST.get('contact_email')
-    contact_phone = request.POST.get('contact_phone')
-    contact_title = request.POST.get('contact_title')
-    new_contact = Company(name=contact_name, phone=contact_phone, email=contact_email, title=contact_title, associated_card=card)
-    new_contact.save()
-    return JsonResponse({'error_message': 1}, safe=False)
+    try:
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        add_name = info['add_name']
+        add_email = info['add_email']
+        add_phone = info['add_phone']
+        card = Card.objects.get(card_id=card_id)
+        add_contact = Contact(name= "," + add_name, email=add_email, phone=add_phone, associated_card=card)
+        add_contact.save()
+        return JsonResponse({'error_message': 1}, safe=False)
+    except Card.DoesNotExist:
+        return JsonResponse({'error_message': -8}, safe=False)
+    except Contact.DoesNotExist:
+        return JsonResponse({'error_message': -14}, safe=False)
 
 # Add tags based on card id and tag names
 @csrf_exempt
 def add_tag(request):
-    card_id = request.GET.get('card_id')
-    card = Card.objects.filter(unique_id=card_id)
-    tags = request.POST.get('tags').split(',')
+    info = json.loads(request.POST.keys()[0])
+    card_id = info['card_id']
+    card = Card.objects.get(card_id=card_id)
+    tags = info['tags'].split(',')
     for tag in tags:
         new_tag = Tag(tag=tag, tagged_card=card)
         new_tag.save()
     return JsonResponse({'error_message': 1}, safe=False)
 
 # Remove tag or set of tags, given a card id and tag name(s)
+@csrf_exempt
 def remove_tag(request):
     try:
-        card_id = request.GET.get('card_id')
-        tags = request.DELETE.get('tags').split(',')
-        for tag in tags:
-            Tag.objects.filter(tag=tag, tagged_card=card_id).delete()
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        card = Card.objects.get(card_id=card_id)
+        Tag.objects.filter(tagged_card=card, tag=info['old_tag']).delete()
         return JsonResponse({'error_message': 1}, safe=False)
     except Card.DoesNotExist:
         return JsonResponse({'error_message': -8}, safe=False)
@@ -189,7 +242,7 @@ def remove_tag(request):
 def get_tags(request):
     try:
         card_id = request.GET.get('card_id')
-        card = Card.objects.filter(unique_id=card_id)
+        card = Card.objects.filter(card_id=card_id)
         tags = Tag.objects.all().filter(tagged_card=card)
         tags_output = serializers.serialize("json", tags)
         return JsonResponse(tags_output, safe=False)
@@ -202,41 +255,71 @@ def get_tags(request):
 @csrf_exempt
 def modify_tag(request):
     try:
-        card_id = request.GET.get('card_id')
-        old_tag = request.GET.get('old_tag')
-        new_tag = request.POST.get('new_tag')
-        card = Card.objects.filter(unique_id=card_id)
-        tags = Tag.objects.filter(tagged_card=card_id)
-        for tag in tags:
-            if tag == old_tag:
-                tag = new_tag
-                tag.save()
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        new_tag = info['new_tag']
+        card = Card.objects.get(card_id=card_id)
+        card.tag = new_tag
+        card.save()
         return JsonResponse({'error_message': 1}, safe=False)
     except Card.DoesNotExist:
         return JsonResponse({'error_message': -8}, safe=False)
-    except Tag.DoesNotExist:
-        return JsonResponse({'error_message': -3}, safe=False)
+    return JsonResponse({'error_message': 1}, safe=False)
+
+
+       # card_id = info('card_id')
+        #old_tag = request.GET.get('old_tag')
+        #new_tag = request.POST.get('new_tag')
+        #card = Card.objects.filter(unique_id=card_id)
+        #tags = Tag.objects.filter(tagged_card=card_id)
+        #for tag in tags:
+        #    if tag == old_tag:
+        #        tag = new_tag
+         #       tag.save()
+        #return JsonResponse({'error_message': 1}, safe=False)
+    #except Card.DoesNotExist:
+     #   return JsonResponse({'error_message': -8}, safe=False)
+    #except Tag.DoesNotExist:
+     #   return JsonResponse({'error_message': -3}, safe=False)
 
 # Change status based on card id and new status
 @csrf_exempt
 def modify_card_status(request):
-    card_id = request.GET.get('card_id')
-    new_status = request.POST.get('status')
-    card = Card.objects.filter(unique_id=card_id)
-    if new_status != card.status:
+    try:
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        new_status = info['new_status']
+        card = Card.objects.get(card_id=card_id)
         card.status = new_status
         card.save()
+        return JsonResponse({'error_message': 1}, safe=False)
+    except Card.DoesNotExist:
+        return JsonResponse({'error_message': -8}, safe=False)
     return JsonResponse({'error_message': 1}, safe=False)
 
+# Edit the notes for a company.
+@csrf_exempt
+def edit_notes(request):
+    try:
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        new_notes = info['new_notes']
+        card = Card.objects.get(card_id=card_id)
+        card.notes = new_notes
+        card.save()
+        return JsonResponse({'error_message': 1}, safe=False)
+    except Card.Exception:
+        return JsonResponse({'error_message': -22}, safe=False)
 
-#class UserProfile(models.Model):
-#    user = models.OneToOneField(User, related_name="profile")
-#
-#    def __str__(self):
-#        return "%s's profile" % self.user
-#
-#def create_user_profile(sender, instance, created, **kwargs):
-#    if created:
-#        profile, created = user.models.UserProfile.objects.get_or_create(user=instance)
-#
-#post_save.connect(create_user_profile, sender=User)
+@csrf_exempt
+def add_task(request):
+    try:
+        info = json.loads(request.POST.keys()[0])
+        card_id = info['card_id']
+        new_task = info['new_task']
+        card = Card.objects.get(card_id=card_id)
+        add_task = Task(task=new_task, associated_card=card)
+        add_task.save()
+        return JsonResponse({'error_message': 1}, safe=False)
+    except Task.Exception:
+        return JsonResponse({'error_message': -16}, safe=False)
