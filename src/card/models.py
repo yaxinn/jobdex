d538744d2e09392ad286958ad6b280fa4c821b83
Paulina Ramos <m.paulina.ramos@gmail.com>
2014-11-22 20:41:43 -0800

    Fix post-integration bugs

--- src/card/models.py
+++ src/card/models.py
@@ -7,7 +7,6 @@ class Company(models.Model):
 
 class Deck(models.Model):
     unique_id = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
-<<<<<<< HEAD
     associated_company = models.OneToOneField(Company)
     owner = models.ForeignKey('user.UserProfile')
 
@@ -25,16 +24,6 @@ class Card(models.Model):
 
 
 
-=======
-    status = models.CharField(max_length=20, blank=False, default="Interested")
-    job_title = models.CharField(max_length=255, blank=False)
-    associated_company = models.ForeignKey(Company)
-    notes = models.TextField(default="")
-    owner = models.ForeignKey('user.UserProfile')
-
-    def __str__(self):
-        return "A card for " + self.status
->>>>>>> 2af7208e384aaf04ca959db7c1e0ae0d37a15dde
 
 class Tag(models.Model):
     tag = models.CharField(max_length=100)
@@ -58,8 +47,4 @@ class Task(models.Model):
     associated_card = models.ForeignKey(Card)
 
     def __str__(self):
-<<<<<<< HEAD
         return "Tasks for " + self.associated_card.name
-=======
-        return "Tasks for " + self.associated_card.name
->>>>>>> 2af7208e384aaf04ca959db7c1e0ae0d37a15dde
